61f8c897a20433c1daa1aa819cf130fc1da4dccd
JxxHxxx <leesin5498@naver.com>
2023-01-27 14:49:35 +0900

    CI/CD TEST 7 - 테스트코드 추가

---
+++ src/test/java/com/jxx/githubactioneb/SimpleTest.java
@@ -0,0 +1,17 @@
+package com.jxx.githubactioneb;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SimpleTest {
+
+    @DisplayName("테스트할게요.")
+    @Test
+    void test() {
+        String s = "hello";
+        Assertions.assertThat(s).isEqualTo("hello");
+    }
+}
