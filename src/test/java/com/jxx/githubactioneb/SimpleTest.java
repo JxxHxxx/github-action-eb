15a2b21c12d63b14fdd576426801ba97fa68071f
JxxHxxx <leesin5498@naver.com>
2023-01-27 14:52:36 +0900

    CI/CD TEST 7 - 테스트코드 추가
    AutoConfigureMockMvc 애노테이션 추가

--- src/test/java/com/jxx/githubactioneb/SimpleTest.java
+++ src/test/java/com/jxx/githubactioneb/SimpleTest.java
@@ -3,8 +3,10 @@ package com.jxx.githubactioneb;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@AutoConfigureMockMvc
 @SpringBootTest
 public class SimpleTest {
 
